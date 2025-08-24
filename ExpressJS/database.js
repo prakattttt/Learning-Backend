6ebb7b1ef0e2a66d2bdde305c7a939f0ad21bcef
Prakat <prakat.dhakall64@gmail.com>
2025-08-24 11:04:59 +0545

    Learning ExpressJS

---
+++ ExpressJS/database.js
@@ -0,0 +1,14 @@
+const database = [
+  { name: "Prakat", age: 21 },
+  { name: "Aarav", age: 25 },
+  { name: "Maya", age: 19 },
+  { name: "Sita", age: 30 },
+  { name: "Ravi", age: 27 },
+  { name: "Anisha", age: 23 },
+  { name: "Kiran", age: 22 },
+  { name: "Deepak", age: 28 },
+  { name: "Nisha", age: 20 },
+  { name: "Rohan", age: 24 }
+];
+export  {database };
+
